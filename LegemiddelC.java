62f8d7a010052f3f5d0509f9cde9d34cca7760d5
imberge <imberge@student.uio.no>
2015-03-13 14:09:31 +0100

    Update LegemiddelC.java

--- LegemiddelC.java
+++ LegemiddelC.java
@@ -3,8 +3,8 @@ public class LegemiddelC extends Legemiddel{
     // Subklasse av Legemidler for legemidler som ikke er narkotiske 
     // eller vanedannende. Inneholder ingen ekstra informasjon. 
 
-    public LegemiddelC(String navn, int pris, int id, double virk){
-        super(navn, pris, id, virk);
+    public LegemiddelC(String navn, int pris, double virk){
+        super(navn, pris, virk);
     }
 
 }
