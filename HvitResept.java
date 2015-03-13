bf073ea88554d643efe4a8dc45ab02cc313847ff
imberge <imberge@student.uio.no>
2015-03-13 12:33:52 +0100

    Update HvitResept.java

--- HvitResept.java
+++ HvitResept.java
@@ -2,7 +2,7 @@ class HvitResept extends Resept{
     
     // Hvit resept extends Resept fordi det er en type resept
     
-    public Resept(int reseptnr, Legemiddel hvilket, Lege hvilken, int pasientid, int reit){
+    public HvitResept(int reseptnr, Legemiddel hvilket, Lege hvilken, int pasientid, int reit){
         super(reseptnr, hvilket, hvilken, pasientid, reit);    
     }
 
