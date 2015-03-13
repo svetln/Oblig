7b640036dd4d750f16165ad5d9b5747987aa74a5
imberge <imberge@student.uio.no>
2015-03-13 12:28:20 +0100

    Update AbstraktSortertEnkelListe.java

--- AbstraktSortertEnkelListe.java
+++ AbstraktSortertEnkelListe.java
@@ -1,9 +1,11 @@
-public interface AbstraktSortertEnkelListe<T extends Comparable<T> & Lik>{
+import java.util.*;
+public interface AbstraktSortertEnkelListe<T extends Comparable<T> & Lik> extends Iterable<T>{
     
     // et interface kan ikke implementere et annet interface fordi et interface aldri
     // selv blir implementert, det kan derfor bare extende et annet interface. 
    
     public boolean settInn(T t);
     public T finnObjekt(String nokkel);
+    public Iterator<T> iterator();
 
 }
