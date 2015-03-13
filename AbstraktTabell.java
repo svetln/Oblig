187601e9fc9f0473ab687c0d34fa656b823a0602
imberge <imberge@student.uio.no>
2015-03-13 12:18:12 +0100

    Update AbstraktTabell.java

--- AbstraktTabell.java
+++ AbstraktTabell.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public interface AbstraktTabell<T extends Iterable>{
+public interface AbstraktTabell<T> extends Iterable<T>{
 
     public boolean settInn(T objekt, int plass);
     public T finnObjekt(int indeks);
