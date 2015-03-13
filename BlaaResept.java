aebef01f1c4fba0dd5f39a1f8b743a971a3d70f2
imberge <imberge@student.uio.no>
2015-03-13 12:33:22 +0100

    Update BlaaResept.java

--- BlaaResept.java
+++ BlaaResept.java
@@ -2,7 +2,7 @@ class BlaaResept extends Resept{
     
     // Blå resept extends Resept fordi det er en type resept. 
 
-    public Resept(int reseptnr, Legemiddel hvilket, Lege hvilken, int pasientid, int reit){
+    public BlaaResept(int reseptnr, Legemiddel hvilket, Lege hvilken, int pasientid, int reit){
        super(reseptnr, hvilket, hvilken, pasientid, reit);
     }
 
